69b8d614139b4f88a9d3b0028c01b3c6a7b57a65
patrykkubala <patrykkubala1999@gmail.com>
2022-07-08 17:01:57 +0200

    addded assets and changed scss

--- content.js
+++ content.js
@@ -7,17 +7,38 @@ let btnDypl = document.querySelector("#btnDypl");
 
 let content = document.querySelector("#content");
 
+function Selected(props) {
+  document.getElementById("btnMaloForm").style.backgroundColor = "";
+  document.getElementById("btnWielkoForm").style.backgroundColor = "";
+  document.getElementById("btnBind").style.backgroundColor = "";
+  document.getElementById("btnDypl").style.backgroundColor = "";
+
+  document.getElementById("btnMaloForm").style.color = "";
+  document.getElementById("btnWielkoForm").style.color = "";
+  document.getElementById("btnBind").style.color = "";
+  document.getElementById("btnDypl").style.color = "";
+  document.getElementById(props).style.backgroundColor = "white";
+  document.getElementById(props).style.color = "#000227";
+}
+
+content.innerHTML = MaloForm;
+Selected("btnMaloForm");
+
 btnMaloForm.addEventListener("click", () => {
   content.innerHTML = MaloForm;
+  Selected("btnMaloForm");
 });
 
 btnWielkoForm.addEventListener("click", () => {
   content.innerHTML = WielkoForm;
+  Selected("btnWielkoForm");
 });
 
 btnBind.addEventListener("click", () => {
   content.innerHTML = Bind;
+  Selected("btnBind");
 });
 btnDypl.addEventListener("click", () => {
   content.innerHTML = Dypl;
+  Selected("btnDypl");
 });
